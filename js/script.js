2b5610334118549da90307481f14b8e63f37ff62
MrCherry89 <abgarnati@yandex.ru>
2025-07-24 19:26:07 +0400

    ok

---
+++ js/script.js
@@ -0,0 +1,55 @@
+
+function switchTab(index) {
+    const buttons = document.querySelectorAll('.tab-button');
+    const contents = document.querySelectorAll('.tab-content');
+
+    buttons.forEach((btn, i) => {
+      btn.classList.toggle('active', i === index);
+      contents[i].classList.toggle('active', i === index);
+    });
+  }
+
+  const swiper = new Swiper(".businessSwiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+    },
+    on: {
+      init: function () {
+        togglePagination(this);
+      },
+      resize: function () {
+        togglePagination(this);
+      },
+    },
+  });
+  
+  function togglePagination(swiperInstance) {
+    if (window.innerWidth >= 768) {
+      if (swiperInstance.pagination.el) {
+        swiperInstance.pagination.el.style.display = 'none';
+      }
+    } else {
+      if (swiperInstance.pagination.el) {
+        swiperInstance.pagination.el.style.display = 'block';
+      }
+    }
+  }
+
+  $(".drop-menu-main").on("click", function(){
+    $(".menu-wrap-main").addClass("open");
+    $("html, body").addClass("overflow");
+  })
+
+  $(".menu-wrap-main .close-menu").on("click", function(){
+    $(".menu-wrap-main").removeClass("open");
+    $("html, body").removeClass("overflow");
+  })
